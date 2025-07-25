f3cecd303fac46f78717b22a30f749780db7e26a
Malcolm <kagsmalcolm@gmail.com>
2025-07-25 17:27:49 +0300

    Add css styling

--- goal-hub/src/App.jsx
+++ goal-hub/src/App.jsx
@@ -3,18 +3,19 @@ import GoalList from "./components/GoalList";
 import GoalForm from "./components/GoalForm";
 import DepositForm from "./components/DepositForm";
 import Overview from "./components/Overview";
+import "./App.css";
 
 function App() {
   const [goals, setGoals] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3001/goals")
+    fetch("https://smart-goal-planner-api-ukrw.onrender.com/goals")
       .then((res) => res.json())
       .then((data) => setGoals(data));
   }, []);
 
   const addGoal = (newGoal) => {
-    fetch("http://localhost:3001/goals", {
+    fetch("https://smart-goal-planner-api-ukrw.onrender.com/goals", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newGoal),
@@ -24,7 +25,7 @@ function App() {
   };
 
   const updateGoal = (updatedGoal) => {
-    fetch(`http://localhost:3001/goals/${updatedGoal.id}`, {
+    fetch(`https://smart-goal-planner-api-ukrw.onrender.com/goals/${updatedGoal.id}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(updatedGoal),
@@ -36,13 +37,13 @@ function App() {
   };
 
   const deleteGoal = (id) => {
-    fetch(`http://localhost:3001/goals/${id}`, {
+    fetch(`https://smart-goal-planner-api-ukrw.onrender.com/goals/${id}`, {
       method: "DELETE",
     }).then(() => setGoals(goals.filter((goal) => goal.id !== id)));
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Smart Goal Planner</h1>
       <GoalForm onAddGoal={addGoal} />
       <DepositForm goals={goals} onDeposit={updateGoal} />
